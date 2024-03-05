f07f1f302d9c1c669a6c75a2030709e40d07d3d7
Sh7yk <93066307+Sh7yk@users.noreply.github.com>
2024-03-05 23:09:18 +0700

    Update WPS_Pixie_auto.sh

--- WPS_Pixie_auto.sh
+++ WPS_Pixie_auto.sh
@@ -10,6 +10,6 @@ INTERFACE=$1
 while (true)
 do
 killall wifite;
-wifite --kill -i $INTERFACE -p 7 --wps-only --random-mac -ic --pixie --ignore-locks --wps-time 30 --wps-fails 1 --wps-timeouts 1;
+wifite --kill -i $INTERFACE -p 7 --wps-only --random-mac --pixie --ignore-locks --wps-time 30 --wps-fails 1 --wps-timeouts 1;
 sleep 1;
 done;
