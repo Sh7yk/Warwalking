e3e4a0ef23be5df0d59bf03bbd5aa78cd96f5473
Sh7yk <93066307+Sh7yk@users.noreply.github.com>
2024-03-05 23:10:07 +0700

    Update WPA_hs_capture.sh

--- WPA_hs_capture.sh
+++ WPA_hs_capture.sh
@@ -10,6 +10,6 @@ INTERFACE=$1
 while (true)
 do
 killall wifite;
-wifite --kill -i $INTERFACE -p 8 --wpa --random-mac --skip-crack --clients-only -ic --wpadt 2 --wpat 15 --no-pmkid --no-wps;
+wifite --kill -i $INTERFACE -p 8 --wpa --random-mac --skip-crack --clients-only --wpadt 2 --wpat 15 --no-pmkid --no-wps;
 sleep 1;
 done;
